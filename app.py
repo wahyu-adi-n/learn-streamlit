3c179c7d07d64cd66a39c9060e7ecebd26e1f3bd
Wahyu Adi Nugroho <wahyuadinugrohoasinan@gmai.com>
2022-12-31 21:37:55 +0700

    learn: text elements

--- app.py
+++ app.py
@@ -2,3 +2,27 @@ import streamlit as st
 
 # Getting started with streamlit
 st.write("Hello World!")
+
+# Markdown
+st.markdown("# Hello, bold")
+
+# Title
+st.title("This is a title")
+
+# Header
+st.header("This is a header")
+
+# Sub header
+st.subheader("This is a sub header")
+
+# Caption
+st.caption("This is a caption")
+
+# Code block
+st.code("a = 123")
+
+# Preformatted text
+st.text("This is a preformatted text")
+
+# LaTeX
+st.latex("123\s")

4e38e3010e8e26f61fd11b3a24bc343290f3507d
Wahyu Adi Nugroho <wahyuadinugrohoasinan@gmai.com>
2022-12-31 22:31:42 +0700

    learn: data display elements

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
+import pandas as pd
+from sklearn.datasets import load_breast_cancer
 
-# Getting started with streamlit
+# 1. Getting started with streamlit
 st.write("Hello World!")
 
+# 2. Text Elements
 # Markdown
 st.markdown("# Hello, bold")
 
@@ -26,3 +29,35 @@ st.text("This is a preformatted text")
 
 # LaTeX
 st.latex("123\s")
+
+# 3. Data Display Elements
+
+data, target = load_breast_cancer(return_X_y=True)
+
+# Dataframe
+st.dataframe(data)
+
+# Tables
+st.table(data)
+
+# Metrics
+st.metric("This is a metrics:", 0)
+
+# JSON
+st.json({
+    "mhs1": {
+        'name': 'Wahyu Adi Nugroho',
+        'nim': 'A11.2019.12310',
+        'age': 22,
+    },
+    "mhs2": {
+        'name': 'Slamet Wahyunigsih',
+        'nim': 'A11.2020.12310',
+        'age': 21,
+    },
+    "mhs3": {
+        'name': 'Dharma Edi Sanjaya',
+        'nim': 'A11.2019.12311',
+        'age': 20,
+    }
+})
